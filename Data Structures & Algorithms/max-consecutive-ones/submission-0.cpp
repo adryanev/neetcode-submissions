bd734d3ce9e8b3fd5d04993586cb3183cce12b85
Adryan Eka Vandra <adryanekavandra@gmail.com>
2026-04-27 11:47:34 +0700

    Add: max-consecutive-ones - submission-0

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+   public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        // the array is binary so we should loop through the array, have a counter
+        // if we found 0 then reset the counter.
+        // also need a variable to store the highest consecutive number
+
+        int max = 0;
+        int counter = 0;
+        int length = nums.size();
+
+        for (int i = 0; i < length; i++) {
+            if (nums[i] == 0) {
+               
+                counter = 0;
+                continue;
+            }
+
+            counter++;
+            max = std::max(max, counter);
+        }
+
+        return max;
+    }
+};
