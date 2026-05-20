23358908a51ccb1c70ad38349cd1206c3518c6d3
Adryan Eka Vandra <adryanekavandra@gmail.com>
2026-05-20 16:42:40 +0700

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        resultArray: List[int] = []
+
+        for operation in operations:
+            if operation == "+":
+                secondValue = resultArray[-1]
+                firstValue = resultArray[-2]
+
+                result = firstValue + secondValue
+
+                resultArray = [*resultArray,result]
+
+                continue
+
+            if operation == "C":
+                resultArray.pop()
+                continue
+
+            if operation == "D":
+                variable = resultArray[-1]
+                value = int(variable) * 2
+
+                resultArray = [*resultArray,  value]
+
+                continue
+
+            if operation.isdecimal:
+                resultArray.append(int(operation))
+                continue
+
+        return sum(resultArray)
